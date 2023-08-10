cc99ed8e88a02369c0ea09fbcff83af2cb528731
Hashir Ali Awan <malikhashiraliawan@gmail.com>
2023-08-10 14:58:40 +0500

    api data calling using model class

--- lib/Activities/home.dart
+++ lib/Activities/home.dart
@@ -12,20 +12,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  getData() async{
-    Uri url=Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=981719515a4bd2e4d4feb88a720a8774");
-    Response response=await get(url);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = json.decode(response.body); // Decode JSON
-      print(data);
-    } else {
-      print("Request failed with status: ${response.statusCode}");
-    }
-  }
+
   int conter=1;
   @override
   void initState() {
-    getData();
     super.initState();
   }
   @override
@@ -38,10 +28,8 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body:Column(
         children: [
-          FloatingActionButton(onPressed: ()=>setState(() {
-            conter+=1;
-          })),
-          Text("$conter"),
+          FloatingActionButton(onPressed: null),
+          Text("kuch nahin babe"),
         ],
       )
     );
--- lib/Activities/loading.dart
+++ lib/Activities/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/Model/model.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +9,16 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void startApp()async{
+    Model model=Model(getlat: 44.34, getlang: 10.99);
+   await model.getData();
+    print(model.air_speed);
+  }
+  @override
+  void initState() {
+    startApp();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
